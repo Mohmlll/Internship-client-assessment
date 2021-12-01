523bc5b99579fd4cb21bcbf0e01f3138a5b1b9a9
Malloum003 <mohammed.malloul@hva.nl>
2021-12-01 12:25:06 +0100

    anagram input and output are done
    Styling is still missing with both assignments, but they both work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from "./components/main/main.component";
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 import {DetailComponent} from "./components/detail/detail.component";
+import {AnagramComponent} from "./components/anagram/anagram.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/landing', pathMatch: 'full'},
@@ -11,6 +12,7 @@ const routes: Routes = [
       {path: ':objectnumber', component: DetailComponent},
     ]
   },
+  {path: 'anagram', component: AnagramComponent},
   {path: 'landing', component: LandingPageComponent},
 
   {path: '**', component: LandingPageComponent}
--- src/app/components/anagram/anagram.component.ts
+++ src/app/components/anagram/anagram.component.ts
@@ -6,11 +6,42 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./anagram.component.scss']
 })
 export class AnagramComponent implements OnInit {
+  public output: any;
+  //default assignment value
+  public input: string = "rope, pore, repo, red rum, murder, listen, silent, endeavor";
 
   constructor() {
+
   }
 
   ngOnInit(): void {
+
   }
 
+  log() {
+    //splits eachs word of the input and adds it to an array
+    let input_array = this.input.split(",");
+    console.log("input is: " + this.input);
+    this.output = this.getAnagrams(input_array)
+    console.log("output is: ");
+    console.log(this.output);
+  }
+
+  
+  private getAnagrams(input: string[]): Array<string[]> {
+    const sorted: { [key: string]: string[] | undefined } = {};
+    for (const word of input) {
+      // Sort the word's letters to be in alphabetical order
+      const sorted_word = word.replace(/ /g, "").split("").sort().join("");
+      // Check if word's order exists in the obj
+      if (sorted[sorted_word]) {
+        sorted[sorted_word]!.push(word);
+      } else {
+        sorted[sorted_word] = [word];
+      }
+    }
+    return Object.keys(sorted).map((key) => {
+      return sorted[key]!;
+    });
+  }
 }
